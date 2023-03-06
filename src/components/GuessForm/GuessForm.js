750c8e0ee6f6ec3b3aaf505d7d4fa31e4e35c735
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2023-03-06 22:02:19 +0000

    Exercise 1

---
+++ src/components/GuessForm/GuessForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function GuessForm() {
+  const [guess, setGuess] = React.useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.info({guess});
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={(e) => {handleSubmit(e)}}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" pattern="[A-Z]{5}" value={guess} onChange={(e) => {setGuess(e.target.value.toUpperCase())}} />
+    </form>
+    );
+}
+
+export default GuessForm;
